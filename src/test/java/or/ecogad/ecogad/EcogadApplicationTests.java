f4a0bd77e1e38a5365a88c22855eadfca95b3afa
gdbs1107 <gdbs1107@naver.com>
2026-02-19 18:10:59 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/or/ecogad/ecogad/EcogadApplicationTests.java
@@ -0,0 +1,13 @@
+package or.ecogad.ecogad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcogadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
